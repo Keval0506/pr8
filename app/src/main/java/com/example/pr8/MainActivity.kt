ea425149b40ae65f6c61fc309c13ec7bc63a4c80
Keval0506 <114097127+Keval0506@users.noreply.github.com>
2023-10-05 11:28:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pr8/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pr8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+    }
+}
